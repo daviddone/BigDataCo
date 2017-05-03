38d8fdbd321fee76bca8551c4a6f37da45409985
daviddone <johnswdaviddone@163.com>
2017-05-03 08:30:51 +0800

    文件字数统计 去重

---
+++ spark/sparkRDD/WholeWordCount.scala
@@ -0,0 +1,16 @@
+import org.apache.spark.{SparkConf, SparkContext}
+//原始数据格式：zhangsan	lisi	lisi	zhangsan	wangwu
+object WholeWordCount {
+  
+  def main(args: Array[String]): Unit = {
+    println("say love me.")
+    val conf = new SparkConf().setAppName("WordCount").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val textRDD = sc.textFile("D:\\david_work\\scala_work\\scala_mro_parser\\src\\wc.txt")
+    val words = textRDD.flatMap { x => x.split("\t") };
+    println("words count distinct"+words.distinct().count()) 
+    println("words count "+words.count()) 
+    val tmpRdd = textRDD.filter(_.contains("zhang"))
+    println("contains str words count "+tmpRdd.count())
+  }
+}
