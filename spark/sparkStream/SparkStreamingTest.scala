70f313cd6a0d4c649a0c8f8633d64c18779ec638
daviddone <johnswdaviddone@163.com>
2017-05-03 11:15:59 +0800

    spark 流式数据处理

---
+++ spark/sparkStream/SparkStreamingTest.scala
@@ -0,0 +1,18 @@
+import org.apache.spark._
+import org.apache.spark.streaming._
+
+object MyFirstStreamingTest {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
+    val ssc = new StreamingContext(conf, Seconds(10))
+    val lines = ssc.socketTextStream("localhost", 9999) //windows nc -L -p 9999 -v
+    val words = lines.flatMap(_.split(" "))
+    val pairs = words.map(word => (word, 1))
+    val wordCounts = pairs.reduceByKey(_ + _)
+    wordCounts.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+
+  }
+}
---
+++ spark/sparkStream/readme.md
@@ -0,0 +1,6 @@
+1  启动SparkStreamingTest.scala
+
+2  windows下使用nc工具发送socket数据
+   步骤一：下载nc
+   步骤二： 切换到nc安装路径，如：cd C:\Users\Administrator\Desktop\netcat-1.11
+   步骤三： 输入命令 nc -L -p 9999 -v ，发送数据
