c48bc66a2fa5c4b1cb03ade3a0f41b0c02141446
unknown <johnswdaviddone@163.com>
2017-06-05 08:32:02 +0800

    搜索引擎 关键字查询

--- elasticsearch/esTest/src/main/java/com/david/es/esUtil.java
+++ elasticsearch/esTest/src/main/java/com/david/es/esUtil.java
@@ -2,14 +2,26 @@ package com.david.es;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.apache.lucene.queryparser.flexible.core.util.StringUtils;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.text.Text;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.highlight.HighlightField;
 
 import com.david.es.bean.Info;
 
@@ -51,6 +63,59 @@ public class esUtil {
 		hashMap.put("detail_href", info.getDetail_href());
 		
 		IndexResponse response = getClient().prepareIndex(index, type).setSource(hashMap).execute().actionGet();
+		//response.getIndex()  response.getType() response.getVersion() response.isCreated()
 		return response.getId();
+	}
+	
+	public static Map<String, Object> search(String key,String index,String type,int start,int row){
+		SearchRequestBuilder builder = getClient().prepareSearch(index);
+		builder.setTypes(type);
+		builder.setFrom(start);
+		builder.setSize(row);
+		//设置高亮字段名称
+		builder.addHighlightedField("title");
+		builder.addHighlightedField("desc");
+		//设置高亮前缀
+		builder.setHighlighterPreTags("<font color='red' >");
+		//设置高亮后缀
+		builder.setHighlighterPostTags("</font>");
+		builder.setSearchType(SearchType.DFS_QUERY_THEN_FETCH);
+		if(!"".equals(key)){
+			builder.setQuery(QueryBuilders.multiMatchQuery(key, "title","desc"));
+		}
+		builder.setExplain(true);//TODO 中文模糊查询。
+		SearchResponse searchResponse = builder.get();
+		
+		SearchHits hits = searchResponse.getHits();
+		long total = hits.getTotalHits();
+		Map<String, Object> map = new HashMap<String,Object>();
+		SearchHit[] hits2 = hits.getHits();
+		map.put("count", total);
+		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		for (SearchHit searchHit : hits2) {
+			Map<String, HighlightField> highlightFields = searchHit.getHighlightFields();
+			HighlightField highlightField = highlightFields.get("title");
+			Map<String, Object> source = searchHit.getSource();
+			if(highlightField!=null){
+				Text[] fragments = highlightField.fragments();
+				String name = "";
+				for (Text text : fragments) {
+					name+=text;
+				}
+				source.put("title", name);
+			}
+			HighlightField highlightField2 = highlightFields.get("desc");
+			if(highlightField2!=null){
+				Text[] fragments = highlightField2.fragments();
+				String desc = "";
+				for (Text text : fragments) {
+					desc+=text;
+				}
+				source.put("desc", desc);
+			}
+			list.add(source);
+		}
+		map.put("dataList", list);
+		return map;
 	}
 }
--- elasticsearch/esTest/src/main/java/com/david/es/esUtilTest.java
+++ elasticsearch/esTest/src/main/java/com/david/es/esUtilTest.java
@@ -1,5 +1,7 @@
 package com.david.es;
 
+import java.util.Map;
+
 import com.david.es.bean.Info;
 
 
@@ -10,8 +12,10 @@ public class esUtilTest
     	esUtil.get();
     	String index = "cq";
     	String type = "info";
-    	Info info = new Info("标题 测试1", "内容描述", "www.test.com");
+    	Info info = new Info("张三丰", "内容描述", "www.test.com");
     	String id = esUtil.addIndexByObject(index, type, info);
     	System.out.println(id);
+    	Map<String, Object> map = esUtil.search("张三丰", index, type, 0, 5);
+    	System.out.println(map.toString());
     }
 }
