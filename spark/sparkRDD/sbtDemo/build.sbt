36263177305826b4645bab3b5bee6bcb94498ea6
unknown <johnswdaviddone@163.com>
2017-06-12 11:12:41 +0800

    sbt编译spark项目

---
+++ spark/sparkRDD/sbtDemo/build.sbt
@@ -0,0 +1,7 @@
+name := "sbtDemo"
+
+version := "1.0"
+
+scalaVersion := "2.10.5"
+
+libraryDependencies +=  "org.apache.spark" % "spark-core_2.10" % "1.6.2"
---
+++ spark/sparkRDD/sbtDemo/src/main/scala/WholeWordCount.scala
@@ -0,0 +1,15 @@
+import org.apache.spark.{SparkConf, SparkContext}
+//原始数据格式\n： zhangsan	lisi	lisi	zhangsan	wangwu
+object WholeWordCount {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("WordCount").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val textRDD = sc.textFile("D:\\david_work\\scala_work\\scala_mro_parser\\src\\wc.txt")
+    val words = textRDD.flatMap { x => x.split("\t") };
+    println("words count distinct"+words.distinct().count())
+    println("words count "+words.count())
+    val tmpRdd = textRDD.filter(_.contains("zhang"))
+    println("contains str words count "+tmpRdd.count())
+  }
+}
