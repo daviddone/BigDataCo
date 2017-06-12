458373ac1d2b65a60554072a4a23c16fa854390b
unknown <johnswdaviddone@163.com>
2017-06-12 11:28:25 +0800

    spark项目本地测试  设置log输出等级

--- spark/sparkRDD/sbtDemo/src/main/scala/WholeWordCount.scala
+++ spark/sparkRDD/sbtDemo/src/main/scala/WholeWordCount.scala
@@ -1,15 +1,17 @@
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
 //原始数据格式\n： zhangsan	lisi	lisi	zhangsan	wangwu
 object WholeWordCount {
 
   def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
     val conf = new SparkConf().setAppName("WordCount").setMaster("local[*]")
     val sc = new SparkContext(conf)
     val textRDD = sc.textFile("D:\\david_work\\scala_work\\scala_mro_parser\\src\\wc.txt")
     val words = textRDD.flatMap { x => x.split("\t") };
     println("words count distinct"+words.distinct().count())
-    println("words count "+words.count())
+    println("words all count "+words.count())
     val tmpRdd = textRDD.filter(_.contains("zhang"))
-    println("contains str words count "+tmpRdd.count())
+    println("contains zhangsan str words count "+tmpRdd.count())
   }
 }
